02dec04256be1c8149d6e43f1e1010ba89d4e135
Dmitriy Monakhov <dimamon6032403@gmail.com>
2024-05-25 23:49:57 +0200

    Fixed

--- RF24GatewayRpi/_install_rf24.sh
+++ RF24GatewayRpi/_install_rf24.sh
@@ -53,11 +53,10 @@ then
 fi
 
 if [ -n "$INSTALL_ALL_FORCE" ]; then
-	echo "TEST"
-	read -p "Choose versions to install (Default: Install latest code from master) [y/N]? " CHOOSE_VERSION
-else
 	echo "Skipping interactive option"
 	CHOOSE_VERSION=Y
+else
+	read -p "Choose versions to install (Default: Install latest code from master) [y/N]? " CHOOSE_VERSION
 fi
 
 if [ -n "$INSTALL_ALL_FORCE" ]; then
