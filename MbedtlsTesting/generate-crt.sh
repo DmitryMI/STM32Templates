160391056468f7a16809dabc2df4089ca0fc9639
Dmitriy Monakhov <dimamon6032403@gmail.com>
2024-05-26 14:55:28 +0200

    RSA changed to ECDSA

--- MbedtlsTesting/generate-crt.sh
+++ MbedtlsTesting/generate-crt.sh
@@ -1,16 +1,24 @@
-openssl genrsa -out ca-prk.pem 2048
+# Generate a new ECDSA private key
+openssl ecparam -genkey -name prime256v1 -out ca-prk.pem
 
+# Create a new certificate signing request (CSR) using the ECDSA private key
 openssl req -new -sha256 -key ca-prk.pem -out ca-csr.pem -subj "/C=US/ST=California/L=Los Angeles/O=TLS Testing"
 
-openssl x509 -req -signkey ca-prk.pem -in ca-csr.pem -out ca-cer.pem -days 3650
+# Sign the CSR with the ECDSA private key to create the certificate
+openssl x509 -req -signkey ca-prk.pem -in ca-prk.pem -out ca-cer.pem -days 3650
 
+# Generate a new ECDSA private key
+openssl ecparam -genkey -name prime256v1 -out server-prk.pem
 
-openssl genrsa -out server-prk.pem 2048
-
+# Create a new certificate signing request (CSR) using the ECDSA private key
 openssl req -new -sha256 -key server-prk.pem -out server-csr.pem -subj "/C=US/ST=California/L=Los Angeles/O=TLS Testing/CN=127.0.0.1"
 
+# Display the CSR details
 openssl req -in server-csr.pem -noout -text
 
+# Sign the CSR with the CA's private key to create the server certificate
 openssl x509 -req -sha256 -in server-csr.pem -CA ca-cer.pem -CAkey ca-prk.pem -CAcreateserial -out server-cer.pem -days 365
 
+# Display the server certificate details
 openssl x509 -in server-cer.pem -noout -text
+
