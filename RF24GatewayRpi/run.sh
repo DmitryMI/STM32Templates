23f1af9216531ee4260b1e206f0e0f4205c053c6
Dmitriy Monakhov <dimamon6032403@gmail.com>
2024-06-06 17:58:19 +0200

    Routing fixed (?)

--- RF24GatewayRpi/run.sh
+++ RF24GatewayRpi/run.sh
@@ -7,6 +7,7 @@ sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -A FORWARD -i tun_nrf24 -o wlan0 -m state --state ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -A FORWARD -i wlan0 -o tun_nrf24 -j ACCEPT
 sudo iptables -t nat -A POSTROUTING -o tun_nrf24 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -j MASQUERADE
 
 mkdir -p logs 
 
