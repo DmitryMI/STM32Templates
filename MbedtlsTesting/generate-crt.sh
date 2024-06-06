9cce072cd1109e2defe66abf18cd8fd6c0213eda
Dmitriy Monakhov <dimamon6032403@gmail.com>
2024-06-06 11:54:37 +0200

    Key size increased

--- MbedtlsTesting/generate-crt.sh
+++ MbedtlsTesting/generate-crt.sh
@@ -1,7 +1,7 @@
 set -e
 
 # Generate a new ECDSA private key
-openssl ecparam -genkey -name secp112r1 -out server-prk.pem
+openssl ecparam -genkey -name secp128r1 -out server-prk.pem
 
 # Create a new certificate signing request (CSR) using the ECDSA private key
 openssl req -new -sha1 -key server-prk.pem -out server-csr.pem -subj "/C=US/ST=0/L=0/O=0/CN=127.0.0.1"
@@ -10,7 +10,7 @@ openssl req -new -sha1 -key server-prk.pem -out server-csr.pem -subj "/C=US/ST=0
 openssl req -in server-csr.pem -noout -text
 
 # Sign the CSR with the CA's private key to create the server certificate
-openssl x509 -req -sha1 -in server-csr.pem -CA ca-cer.pem -CAkey ca-prk.pem -CAcreateserial -out server-cer.pem -days 365
+openssl x509 -req -sha1 -in server-csr.pem -CA ca-cer.pem -CAkey ca-prk.pem -CAcreateserial -out server-cer.pem -days 3650
 
 # Display the server certificate details
 openssl x509 -in server-cer.pem -noout -text
